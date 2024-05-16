92f0f7a605f3f10d46843457bfc7174131e5c0cd
Darick33 <110338321+Darick33@users.noreply.github.com>
2024-05-16 00:31:22 -0500

    Update head.php

--- views/html/head.php
+++ views/html/head.php
@@ -34,3 +34,4 @@
 <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
 <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
 <script src="../public/assets/js/config.js"></script>
+
